390cccacd31f064bc138796d4b9f51ce964a7462
Fernando Mojica <fernmarmojica@gmail.com>
2023-08-09 11:55:46 -0500

    Added current level calculation util

--- Utils/Levels.go
+++ Utils/Levels.go
@@ -1,1 +1,15 @@
 package Utils
+
+func CalculateCurrentLevelFromXP(xp int) int {
+	if xp < 100 {
+		return 0
+	} else if xp >= 101 && xp < 224 {
+		return 1
+	} else if xp >= 225 && xp < 374 {
+		return 2
+	} else {
+		levelCalc := xp - 375
+		levelCalc = (levelCalc / 200) + 2
+		return levelCalc
+	}
+}
